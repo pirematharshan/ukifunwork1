26443e984a6d04a253304529ce1cda259ebe5bd6
ISshanviththiya <sshanviththiya@gmail.com>
2019-12-16 14:18:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,124 @@
+
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {  });
+SC.stream('/tracks/284271005',function(sound){ });
+
+$('#start1').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop1').click(function(e) {
+                    e.preventDefault();
+                    sound.stop();
+                  });
+
+
+
+
+
+                  $(document).ready(function() {  });
+                  SC.stream('/tracks/133060071',function(sound){ });
+
+                  $('#start2').click(function(e) {
+                             e.preventDefault();
+                             sound.start();
+                           });
+
+                  $('#stop2').click(function(e) {
+                                      e.preventDefault();
+                                      sound.stop();
+                                    });
+
+
+
+
+                                    $(document).ready(function() {  });
+                                    SC.stream('/tracks/512660685',function(sound){ });
+
+                                    $('#start3').click(function(e) {
+                                               e.preventDefault();
+                                               sound.start();
+                                             });
+
+                                    $('#stop3').click(function(e) {
+                                                        e.preventDefault();
+                                                        sound.stop();
+                                                      });
+
+
+
+                                                      $(document).ready(function() {  });
+                                                      SC.stream('/tracks/399353328',function(sound){ });
+
+                                                      $('#start4').click(function(e) {
+                                                                 e.preventDefault();
+                                                                 sound.start();
+                                                               });
+
+                                                      $('#stop4').click(function(e) {
+                                                                          e.preventDefault();
+                                                                          sound.stop();
+                                                                        });
+
+
+                                                                        $(document).ready(function() {  });
+                                                                        SC.stream('/tracks/259133132',function(sound){ });
+
+                                                                        $('#start5').click(function(e) {
+                                                                                   e.preventDefault();
+                                                                                   sound.start();
+                                                                                 });
+
+                                                                        $('#stop5').click(function(e) {
+                                                                                            e.preventDefault();
+                                                                                            sound.stop();
+                                                                                          });
+
+
+
+                                                                                          $(document).ready(function() {  });
+                                                                                          SC.stream('/tracks/177579936',function(sound){ });
+
+                                                                                          $('#start6').click(function(e) {
+                                                                                                     e.preventDefault();
+                                                                                                     sound.start();
+                                                                                                   });
+
+                                                                                          $('#stop6').click(function(e) {
+                                                                                                              e.preventDefault();
+                                                                                                              sound.stop();
+                                                                                                            });
+
+
+
+                                                                                                            $(document).ready(function() {  });
+                                                                                                            SC.stream('/tracks/271004119',function(sound){ });
+
+                                                                                                            $('#start7').click(function(e) {
+                                                                                                                       e.preventDefault();
+                                                                                                                       sound.start();
+                                                                                                                     });
+
+                                                                                                            $('#stop7').click(function(e) {
+                                                                                                                                e.preventDefault();
+                                                                                                                                sound.stop();
+                                                                                                                              });
+
+
+
+                                                                                                                              $(document).ready(function() {  });
+                                                                                                                              SC.stream('/tracks/255615355',function(sound){ });
+
+                                                                                                                              $('#start8').click(function(e) {
+                                                                                                                                         e.preventDefault();
+                                                                                                                                         sound.start();
+                                                                                                                                       });
+
+                                                                                                                              $('#stop8').click(function(e) {
+                                                                                                                                                  e.preventDefault();
+                                                                                                                                                  sound.stop();
+                                                                                                                                                });
